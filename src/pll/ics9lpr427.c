d2ad00612989995a31df5cf3c15ba208eed66004
Andrey Zhornyak <darion_@users.sourceforge.net>
2014-07-24 00:08:42 +0300

    isc9lpr427 algorithm based reimplementarion

--- src/pll/ics9lpr427.c
+++ src/pll/ics9lpr427.c
@@ -1,7 +1,7 @@
 /*
- *  C Implementation: ics9lpr427
+ *  C Implementation: ics9lpr427.c
  *
- * Description: ics9lpr427
+ * Description: ics9lpr427 PLL driver
  *
  *
  * Author: Andrey Zhornyak <darion76@gmail.com>, (C) 2014
@@ -9,243 +9,140 @@
  * Copyright: See COPYING file that comes with this distribution
  *
  */
-#include "i2c.h"
-#include <math.h>
+#include "alg1.h"
 
 #ifdef DEBUG
 #include <stdio.h>
 #endif
 
-#define BYTECOUNT 31
-#define CMD 0x00
+// N bits definitions
 
-static int FSBIndex = 0;
-static const unsigned int FSB_Min = 100;
-static const unsigned int FSB_Max = 400;
-
-static int ics9lpr427_unhide(const int file)
+const BB_t const N_bm[] =
 {
-	int res;
-	unsigned char buf[BYTECOUNT];
+    {"0",  11, 6},
+    {"1",  11, 7},
+    {"2",  12, 0},
+    {"3",  12, 1},
+    {"4",  12, 2},
+    {"5",  12, 3},
+    {"6",  12, 4},
+    {"7",  12, 5},
+    {"8",  12, 6},
+    {"9",  12, 7},
+    {""}
+};
 
-	res = i2c_smbus_read_block_data(file, CMD, buf);
-	if(res < 0)
-		return -1;
-#ifdef DEBUG
-	else
-		printf("unhide DEBUG: %i bytes read :    ", res);
-		for(int i=0; i<res; i++)
-			printf("%02X ", buf[i]);
-		printf("\n");
-#endif /* DEBUG */
+// M bits definitions
 
-	if(res != 32)
-	{
-		buf[8] = 0x1F;
-		res = i2c_smbus_write_block_data(file, CMD, 13, buf);
-		if(res)
-			return -1;
-#ifdef DEBUG
-			printf("unhide DEBUG: %i bytes written : ", 13);
-			for(int i=0; i<13; i++)
-				printf("%02X ", buf[i]);
-			printf("\n");
-#endif /* DEBUG */
-	}
-	else
-		return 0;
-
-	res = i2c_smbus_read_block_data(file, CMD, buf);
-	if(res < 0)
-		return -1;
-#ifdef DEBUG
-	printf("unhide DEBUG: %i bytes read :    ", res);
-	for(int i=0; i<res; i++)
-		printf("%02X ", buf[i]);
-	printf("\n");
-#endif /* DEBUG */
-
-	if(res == 0x20 && buf[8] == 0x1F)
-	return 0;
-
-	return -1;
-}
-
-
-int ics9lpr427_CheckFSB(int fsb, float *ram, float *pci, float *agp)
+const BB_t const M_bm[] =
 {
-	if(ram)
-		*ram = fsb;
-	if(pci)
-		*pci = -1.0f;
-	if(agp)
-		*agp = -1.0f;
+    {"0", 11, 0},
+    {"1", 11, 1},
+    {"2", 11, 2},
+    {"3", 11, 3},
+    {"4", 11, 4},
+    {"5", 11, 5},
+    {""}
+};
 
-	if(fsb <= FSB_Max && fsb >= FSB_Min)
-		return 0;
+// bc bits definitions
 
-	return -1;
-}
+const BB_t const bc_bm[] =
+{
+    {"0", 8, 0},
+    {"1", 8, 1},
+    {"2", 8, 2},
+    {"3", 8, 3},
+    {"4", 8, 4},
+    {""}
+};
+
+const BB_t const TME_bm[] =
+{
+    {"0", 1, 0},
+    {""}
+};
+
+// Divide table set bits definitions
+
+const BB_t const dt_bm[] =
+{
+    {"0", 28, 0},
+    {"1", 28, 1},
+    {"2", 28, 2},
+    {"3", 28, 3},
+    {""}
+};
 
 static const int const DivTable[] =
 {
-	0x02, 0x03, 0x05, 0x07,
-	0x04, 0x06, 0x0A, 0x0E,
-	0x08, 0x0C, 0x14, 0x1C,
-	0x10, 0x18, 0x28, 0x38
+    0x02, 0x03, 0x05, 0x07,
+    0x04, 0x06, 0x0A, 0x0E,
+    0x08, 0x0C, 0x14, 0x1C,
+    0x10, 0x18, 0x28, 0x38
 };
 
-static int ics9lpr427_CalcFSB(const unsigned int n, const unsigned int m, const unsigned int dividx)
+const alg1_t alg1_ics9lpr427 = 
 {
-	return 125.0f * (float)n / (float)m / (float)DivTable[dividx & 0x0F];
+    "ics9lpr427",    // PLL name
+    &N_bm[0],         // N bitmap
+    &M_bm[0],         // M bitmap
+    &bc_bm[0],        // byte count bitmap
+    &dt_bm[0],        // divide table bitmap
+    &TME_bm[0],       // TME bitmap
+    125.0f,           // M, N ratio (for the real FSB value calculation)
+    31,               // byte count when read in unhide mode / write
+    15,               // byte count when read in hyde mode / write
+    0,                // command
+    1,                // Divide table enable or not
+    &DivTable[0],     // Divide table
+    100,              // FSB_Min
+    400               // FSB_Max
+} ;
+
+
+//-----------------------------------------------------------------------------------------------
+// TODO: need rework main code to implement algorithm based architecture
+
+static int ics9lpr427_unhide(const int file)
+{
+    return alg1_unhide(&alg1_ics9lpr427, file);
+}
+
+int ics9lpr427_CheckFSB(int fsb, float *ram, float *pci, float *agp)
+{
+    return alg1_CheckFSB(&alg1_ics9lpr427, fsb, ram, pci, agp);
+}
+
+static int ics9lpr427_CalcFSB(const unsigned int n, const unsigned int m, const unsigned char * buf)
+{
+    return alg1_CalcFSB(&alg1_ics9lpr427, n, m, buf);
 }
 
 int ics9lpr427_SetFSB(int fsb)
 {
-	int file, res;
-	unsigned int n, m;
-	unsigned char buf[32];
-
-	if(fsb > FSB_Max || fsb < FSB_Min)
-		return -1;
-
-	file = i2c_open();
-	if(file < 0)
-		return -1;
-
-	res = i2c_smbus_read_block_data(file, CMD, buf);
-	if(res != BYTECOUNT)
-	{
-#ifdef DEBUG
-		printf("SetFSB DEBUG: %i (should be %i) bytes read : ", res, BYTECOUNT);
-		for(int i=0; i<res; i++)
-			printf("%02X ", buf[i]);
-		printf("\n");
-#endif /* DEBUG */
-		i2c_close();
-		return -1;
-	}
-#ifdef DEBUG
-	else
-	{
-		printf("SetFSB DEBUG: %i bytes read : ", res);
-		for(int i=0; i<res; i++)
-			printf("%02X ", buf[i]);
-		printf("\n");
-	}
-#endif /* DEBUG */
-
-	n = (buf[12] << 2) | ((buf[11] & 0xC0) >> 6);
-	m = buf[11] & 0x3F;
-	if(ics9lpr427_CheckFSB(ics9lpr427_CalcFSB(n, m, buf[28]), NULL, NULL, NULL))
-	{
-#ifdef DEBUG
-		printf("SetFSB DEBUG: Read FSB out of range.\nStopping.\n");
-#endif /* DEBUG */
-		i2c_close();
-		return -1;
-	}
-
-//	m = 18;
-	n = (fsb * m * DivTable[(buf[28] & 0x0F)])/125;
-
-	buf[11] = m & 0x3F;
-	buf[11] |= (n & 0x03) << 6;
-	buf[12] = (n >> 2) & 0xFF;
-//	buf[28] = 0x00;
-
-	res = i2c_smbus_write_block_data(file, CMD, BYTECOUNT, buf);
-	i2c_close();
-
- if(res < 0)
-	 return -1;
-#ifdef DEBUG
-  else
-		printf("SetFSB DEBUG: %i bytes written : ", BYTECOUNT);
-  for(int i=0; i<BYTECOUNT; i++)
-		printf("%02X ", buf[i]);
-  printf("\n");
-#endif /* DEBUG */
-
-	return 0;
+    return alg1_SetFSB(&alg1_ics9lpr427, fsb);
 }
 
 int ics9lpr427_GetFSB()
 {
-	int file, res;
-	unsigned int n, m;
-	unsigned char buf[32];
-
-	file = i2c_open();
-	if(file < 0)
-		return -1;
-	ics9lpr427_unhide(file);
-	res = i2c_smbus_read_block_data(file, CMD, buf);
-	i2c_close();
-
-	if(res < 0) return -1;
-#ifdef DEBUG
-	else
-	{
-		printf("GetFSB DEBUG: %i bytes read : ", res);
-		for(int i=0; i<res; i++)
-			printf("%02X ", buf[i]);
-		printf("\n");
-	}
-#endif /* DEBUG */
-
-	n = (buf[12] << 2) | ((buf[11] & 0xC0) >> 6);
-	m = buf[11] & 0x3F;
-
-#ifdef DEBUG
-	printf("GetFSB DEBUG: n=%i, m=%i, idx=%i \n", n, m, DivTable[(buf[28] & 0x0F)]);
-#endif /* DEBUG */
-
-
-	return ics9lpr427_CalcFSB(n, m, buf[28]);
+    return alg1_GetFSB(&alg1_ics9lpr427);
 }
 
 int ics9lpr427_GetFirstFSB()
 {
-	FSBIndex = FSB_Min;
-
-	return FSBIndex;
+    return alg1_GetFirstFSB(&alg1_ics9lpr427);
 }
 
 int ics9lpr427_GetNextFSB()
 {
-	FSBIndex++;
-
-	if(FSBIndex <= FSB_Max)
-		return FSBIndex;
-
-	return -1;
+    return alg1_GetNextFSB(&alg1_ics9lpr427);
 }
 
 int ics9lpr427_CheckTME()
 {
-	int file, res;
-	unsigned char buf[BYTECOUNT];
-
-	file = i2c_open();
-	if(file < 0)
-		return -1;
-
-	ics9lpr427_unhide(file);
-	res = i2c_smbus_read_block_data(file, CMD, buf);
-	i2c_close();
-
-#ifdef DEBUG
-	printf("CheckTME DEBUG: %i bytes read : ", res);
-	for(int i=0; i<res; i++)
-		printf("%02X ", buf[i]);
-	printf("\n");
-#endif /* DEBUG */
-	if(res < 0)
-		return -1;
-
-	if(buf[1] & 0x01)
-		return 1;
-
-	return 0;
+    return alg1_CheckTME(&alg1_ics9lpr427);
 }
+
+
+//-----------------------------------------------------------------------------------------------
